9796a70583630af65faaa0c6caf7084588b4cf24
tommy tom <t.stear@gmail.com>
2015-10-27 12:39:15 -0600

    First attempt at reading time from the MSF radio.

--- MSFClock.ino
+++ MSFClock.ino
@@ -3,25 +3,26 @@
 #include <Adafruit_MCP23017.h>
 #include <Adafruit_RGBLCDShield.h>
 
-#include <Time.h>
 #include <DS1307RTC.h>
+#include <Time.h>
 #include <MSFTime.h>
 #include <Wire.h>
 #include <LiquidCrystal.h>
 
 
-#define LCD_ADDR = 0x20;
-#define RTC_ADDR = 0x68;
+#define LCD_ADDR  0x20;
+#define RTC_ADDR  0x68;
+#define RADIO_PIN 8;
 
 
 void setupLCD();
-void setupRTC();
+void setRTC();
 
 void secondsSinceRestart();
-char* getRadioTime();
+void getRadioTime();
+time_t msfTimeSync();
 
 tmElements_t readRTC();
-bool  setRTC();
 
 time_t convertToUnixEpoch(tmElements_t tm);
 
@@ -31,19 +32,31 @@ time_t convertToUnixEpoch(tmElements_t tm);
 // This equates to 0x20 for I2C bus connections, according to the documentation.
 LiquidCrystal lcd(0);
 
+// Initialize the MSF radio circuit
+MSFTime msf;
+time_t prevDisplay = 0;
+byte prevStatus = 255;
+
+
 
 void setup() {
   Serial.begin(38400);
   setupLCD();
+
+  msf.init(255);
+  setSyncProvider(msfTimeSync);
 }
 
 void loop() {
 //  secondsSinceRestart();
+  getRadioTime();
+  
   tmElements_t tm = readRTC();
   lcd.setCursor(0,1);
   lcd.print(convertToUnixEpoch(tm));
   delay(1000);  
 }
+
 
 
 /************************************************************
@@ -91,3 +104,70 @@ time_t convertToUnixEpoch(tmElements_t tm) {
   Serial.println(t);
   return t;
 }
+
+
+
+void getRadioTime() {
+  byte currStatus = msf.getStatus();
+  Serial.print("Radio Status:  ");
+  Serial.println(currStatus);
+  if (currStatus != prevStatus || currStatus & MSF_STATUS_FIX) {
+    if (currStatus != prevStatus) {
+      if (currStatus & MSF_STATUS_CARRIER)
+        Serial.println("Got carrier");
+      if ((currStatus & MSF_STATUS_WAITING))
+        Serial.println("Waiting for minute sync");
+      if ((currStatus & MSF_STATUS_READING))
+        Serial.println("Reading fix"); 
+      prevStatus = currStatus;
+    }
+    
+    now();
+
+    Serial.print("value of now:  ");
+    Serial.println(now());
+    
+    if (timeStatus()!= timeNotSet) {
+      if(now() != prevDisplay) { //update the display only if the time has changed
+        prevDisplay = now();
+        digitalClockDisplay(); 
+        RTC.set(now());
+        lcd.clear();
+        lcd.setCursor(0,0);
+        Serial.println("Setting Time.");
+        lcd.print("Setting time");
+        delay(250);
+        lcd.clear();
+        lcd.print("UNIX Epoch Time!");
+      }
+    }
+  }
+}
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+
+
+time_t msfTimeSync() {
+  return msf.getTime();
+}
+
